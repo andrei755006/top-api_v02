5f00522ee6df36576ed3d43af7d258e7877cbd38
shefmaster <andrej.kozik@gmail.com>
2023-06-01 13:31:50 +0200

    top-page.model added

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from '@m8a/nestjs-typegoose';
+import { TopPageModel } from './top-page.model';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+        TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModel,
+        schemaOptions: {
+          collection: 'TopPage'
+        }
+      }
+    ])
+  ],
   controllers: [TopPageController],
   providers: [ConfigService],
 })
