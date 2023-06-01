21bb335521ddbf0f02e7c35ff95cb1a4463448b4
shefmaster <andrej.kozik@gmail.com>
2023-06-01 13:11:03 +0200

    imports updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { TypegooseModule } from '@m8a/nestjs-typegoose';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModule,
+        schemaOptions: {
+          collection: 'Auth'
+        }
+      }
+    ])
+  ]
 })
 export class AuthModule {}
